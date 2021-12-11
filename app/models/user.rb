854cbda5ab7eb5aad4983a7718e441e9866a7a8e
Mateusz Talkowski <fly_master@onet.eu>
2021-12-11 15:28:51 +0100

    Updated user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates_presence_of :name
+  # validates_presence_of :name
 
   def first_name
-    self.name.split.first
+      self.name.split.first.oops
   end
 
   def last_name
