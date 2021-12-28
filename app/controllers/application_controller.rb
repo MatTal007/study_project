78cbc3811ce9094627cd11d1f560c9a5ed5e1297
Mateusz Talkowski <fly_master@onet.eu>
2021-12-28 16:21:42 +0100

    Integrated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContentConcern
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = MTalkViewTool::Renderer.copyright 'Mateusz Talkowski', 'All rights reserved'
+  end
+end
+
+module MTalkViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
