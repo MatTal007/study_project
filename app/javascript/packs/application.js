3e0d91ca7d1983b44b88b4a1e38b6616ee7ad0ee
Mateusz Talkowski <fly_master@onet.eu>
2022-02-14 15:39:18 +0100

    Implemented partials for navogation, footer and masthead for the portfolio layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,23 +2,28 @@
 //= require turbolinks
 //= require_tree
 //= require jquery3
+//= require jquery
 //= require popper
 //= require bootstrap-sprockets
 //= require jquery_ujs
-//= require jquery'
+//= require jquery
 //= require popper
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-
-
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'jquery-ui'
+ 
+$(
+        () => {
+                $('.sortable').sortable()
+        }
+)
 
 Rails.start()
 Turbolinks.start()
