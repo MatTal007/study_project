1a29105a610583de8adfbe24b7ac0010ab479e00
Mateusz Talkowski <fly_master@onet.eu>
2022-02-14 16:29:04 +0100

    Implemented show page styles for portfolio layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,8 +30,8 @@ puts "5 skills created"
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "Ruby on Rails",
     body: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from 'de Finibus Bonorum et Malorum' by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
-    main_image: "https://placehold.it/600x400",
-    thumb_image: "https://placehold.it/350x200"
+    main_image: "https://place-hold.it/600x400",
+    thumb_image: "https://place-hold.it/350x200"
   )
 end 
 
